79d97aaab74a27752479a027cd78ea8694582734
adithyavenkatesh <adithyav211@gmail.com>
2020-09-26 18:17:28 -0400

    first commit

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,49 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+tableData.forEach(function(data) {
+    console.log(data);
+    
+    var row = tbody.append("tr");
+
+    Object.entries(data).forEach(function([key, value]) {
+      console.log(key, value);
+      
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+var button = d3.select("#filter-btn");
+button.on("click", function() {
+
+    tbody.html("");
+
+    var inputElement = d3.select("#datetime");
+   
+    var inputValue = inputElement.property("value");
+    
+    console.log(inputValue);
+   
+    var filteredData = tableData.filter(data => data.datetime === inputValue);
+
+    console.log(filteredData);
+
+    filteredData.forEach(function(data) {
+
+    console.log(data);
+   
+    var row = tbody.append("tr");
+    
+    Object.entries(data).forEach(function([key, value]) {
+        console.log(key, value);
+      
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+});
+
